3de22fe1d2455687ed3e1125daad3d9e0d060eb1
Tawsif Hossain <tawsifhossain18.o@gmail.com>
2026-06-24 13:45:47 +0600

    Done

--- QUERY.sql
+++ QUERY.sql
@@ -94,3 +94,48 @@ FROM
   bookings
 WHERE
   payment_status IS NULL
+  
+SELECT
+  booking_id,
+  full_name,
+  fixture,
+  total_cost
+FROM
+  bookings
+  INNER JOIN users ON bookings.user_id = users.user_id
+  INNER JOIN matches ON bookings.match_id = matches.match_id
+  
+SELECT
+  users.user_id,
+  full_name,
+  booking_id
+FROM
+  users
+  FULL JOIN bookings ON bookings.user_id = users.user_id
+  
+SELECT
+  booking_id,
+  match_id,
+  total_cost
+FROM
+  bookings
+WHERE
+  total_cost > (
+    SELECT
+      avg(total_cost)
+    FROM
+      bookings
+  )
+  
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+ORDER BY
+  base_ticket_price DESC
+LIMIT
+  2
+OFFSET
+  1
